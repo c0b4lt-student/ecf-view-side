72dc11ba7479040a170b5d68b4cec1ca0d042eb1
c0b4lt-student <student.corentin.turgis@gmail.com>
2022-10-12 20:04:06 +0200

    changing led position

--- src/components/ui/PartnerCard.js
+++ src/components/ui/PartnerCard.js
@@ -6,7 +6,7 @@ import '../../css/led.css';
 function PartnerCard(props) {
   const partner = props.children
   return (
-    <li className="bg-secondary w-75 h-25 m-auto mt-3 mb-3 rounded d-flex flex-column" key={partner.id_partner}>
+    <li className="bg-secondary w-75 m-auto mt-3 mb-3 p-0 rounded d-flex flex-column" key={partner.id_partner}>
       <img src={partner.is_active_partner ? green_light : red_light} alt="Utilisateur actif" className="led-img m-0 p-0"/>
       <h3 className="mx-auto pt-1">{partner.lastname_partner.toUpperCase()} {partner.firstname_partner}</h3>
       <p className="mx-auto">{partner.email_partner}</p>
