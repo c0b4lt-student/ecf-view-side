fc0c23049a7efc256bf625f5561f9a5aae6acfe0
c0b4lt-student <student.corentin.turgis@gmail.com>
2022-10-12 21:34:36 +0200

    adding a perm list in accordio body

--- src/components/ui/PermsPanel.js
+++ src/components/ui/PermsPanel.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import PartnerCard from "./PartnerCard";
 
 function PermsPanel(props) {
   const url = 'https://ecf-decembre-2022.herokuapp.com/?page=front/' + props.type + '/' + props.id_item + '/perms';
   const [perms, setPerms] = useState(null)
-  console.log(url)
   useEffect(() => {
     axios.get(url)
       .then(response => {
@@ -13,7 +13,15 @@ function PermsPanel(props) {
   }, []);
   return (
       <div className="accordion-body bg-dark-secondary w-75 h-25 m-auto mb-3 rounded">
-        {perms}
+        <ul className="m-0 p-0" id="perms-list">
+          {
+            perms ? (perms.map((perm) => {
+              return (
+                <li key={perm.id_perm}>{perm.short_desc_perm}</li>
+              );
+            })) : ''
+          }
+        </ul>
       </div>
   );
 }
